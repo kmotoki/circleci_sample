7abe6367b8a901ea7eed3d6ff022fb9e18ef8578
kiyoshi.motoki <kiyoshi.motoki@gmail.com>
2017-06-05 13:34:51 +0900

    First commit.

---
+++ src/circleci_sample/ClassA.java
@@ -0,0 +1,14 @@
+package circleci_sample;
+
+public class ClassA {
+
+    private String s = null;
+
+    public boolean equailsWith(String text) {
+        if (s.equals(text)) {
+            return true;
+        } else {
+            return false;
+        }
+    }
+}
